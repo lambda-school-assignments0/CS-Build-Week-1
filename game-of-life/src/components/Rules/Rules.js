43af2fbaf0239a43bdc487c256b0905d12cf3e83
schang1146 <sammychang2185@gmail.com>
2020-06-24 16:25:56 -0700

    populated rules section

--- game-of-life/src/components/Rules/Rules.js
+++ game-of-life/src/components/Rules/Rules.js
@@ -6,15 +6,10 @@ class Rules extends Component {
             <div>
                 <h2>Rules:</h2>
                 <ul>
-                    <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas gravida lectus turpis, sed vulputate augue malesuada et. Duis metus lectus, tempor sed finibus nec, feugiat nec nisl.</li>
-                    <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</li>
-                    <li>Lorem ipsum dolor sit amet.</li>
-                    <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</li>
-                    <li>Maecenas gravida lectus turpis, sed vulputate augue malesuada et. Duis metus lectus, tempor sed finibus nec, feugiat nec nisl.</li>
-                    <li>Maecenas gravida lectus turpis, sed vulputate augue malesuada et.</li>
-                    <li>Duis metus lectus, tempor sed finibus nec, feugiat nec nisl.</li>
-                    <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas gravida lectus turpis, sed vulputate augue malesuada et. Duis metus lectus, tempor sed finibus nec, feugiat nec nisl.</li>
-                    <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</li>
+                    <li>Any live cell with fewer than 2 live neighbors dies, as if by underpopulation.</li>
+                    <li>Any live cell with 2 or 3 live neighbors lives on to the next generation.</li>
+                    <li>Any live cell with more than 3 live neighbors dies, as if by overpopulation.</li>
+                    <li>Any dead cell with exactly 3 live neighbors becomes a live cell, as if by reproduction.</li>
                 </ul>
             </div>
         );
